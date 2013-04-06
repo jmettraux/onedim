b17397322fcf1fecdbf87df33fb2049f5dacdac7
John Mettraux <jmettraux@gmail.com>
2013-04-06 14:31:59 +0900

    slow down bin/automaton.rb

--- bin/automaton.rb
+++ bin/automaton.rb
@@ -3,12 +3,15 @@
 $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'onedim'
 
-state, rule = ARGV
+state, rule, freq = ARGV
+
+freq = (freq || 0.4).to_f
 
 a = OneDim::Automaton.new(state, rule.to_i)
 
 loop do
   puts a.state
+  sleep(freq)
   a.step
 end
 
